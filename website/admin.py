8408f40c4dec840ee9ddaf2723df9851770ac87c
Akash Sivanandan <sendboba@protonmail.com>
2021-11-04 14:04:11 +0530

    Registered custom user model to admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from website.models import Author, Book, BookInstance, Genre
+from website.models import Author, Book, BookInstance, Genre, CustomUser
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
+admin.site.register(CustomUser)
