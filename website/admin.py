98af4cb1d2953f5209038a59e7fe01e4e43b4e45
Akash Sivanandan <sendboba@protonmail.com>
2021-11-04 13:01:29 +0530

    Registered the models on admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from website.models import Author, Book, BookInstance, Genre
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
