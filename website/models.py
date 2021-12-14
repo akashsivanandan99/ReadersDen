e27145b1c33ead450750140bf2e5a55400049529
Akash <akash.sivanandan@protonmail.coim>
2021-12-14 22:33:23 +0530

    Remodeled the author template, with a single field for name instead of a separate first name and last name which was making it complicated to handle more diverse names

--- website/models.py
+++ website/models.py
@@ -115,13 +115,12 @@ class BookInstance(models.Model):
 
 class Author(models.Model):
     """Model representing an author."""
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     class Meta:
-        ordering = ['last_name', 'first_name']
+        ordering = ['name']
 
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
@@ -129,4 +128,4 @@ class Author(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.last_name}, {self.first_name}'
+        return f' {self.name}'
