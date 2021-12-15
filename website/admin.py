ce9bc301af99a41a2be1f8e3cc486ecb1df0244a
Akash <akash.sivanandan@protonmail.coim>
2021-12-16 03:25:41 +0530

    Registered the Contact model to the admin site, so that it can be accessed by admin users

--- website/admin.py
+++ website/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from website.models import Author, Book, BookInstance, Genre, CustomUser
+from website.models import Author, Book, BookInstance, Genre, CustomUser, Contact
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(CustomUser)
+admin.site.register(Contact)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author',)
-
