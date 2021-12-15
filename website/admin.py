17c137b27261e187bf84ad5ff403256b391f79ab
Akash <akash.sivanandan@protonmail.coim>
2021-12-16 02:02:31 +0530

    Changed the list_display for the Book admin class

--- website/admin.py
+++ website/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from website.models import Author, Book, BookInstance, Genre, CustomUser
 
-admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(CustomUser)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+
